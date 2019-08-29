177c47110f1d5d9f28d6a9bd1c373a7dee79c42a
mark9064 <30447455+mark9064@users.noreply.github.com>
2019-08-29 22:52:22 +0100

    Switch travis build to manylinux

---
+++ ci/travis_build_wheels.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e -x
+
+# Install librdrm developement headers
+yum install -y libdrm-devel
+
+# Setup python versions to be built
+declare -a python_versions
+python_versions=(cp35-cp35m cp36-cp36m cp37-cp37m)
+
+# Install dependencies and compile wheels
+for version in "${python_versions[@]}"; do
+    /opt/python/$version/bin/pip install -r /io/dev-requirements.txt
+    /opt/python/$version/bin/pip wheel /io/ -w dist-wip/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in dist-wip/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/dist/
+done
+
+# Install wheels
+for version in "${python_versions[@]}"; do
+    /opt/python/$version/bin/pip install pyamdgpuinfo --no-index -f /io/dist
+done
