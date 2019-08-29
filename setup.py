88601b440d9012f6a2480967a6aa0349d9adb348
mark9064 <30447455+mark9064@users.noreply.github.com>
2019-08-29 23:43:49 +0100

    Add MANIFEST.in to ensure cython files are included, 1.0.2 release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ EXTENSIONS = [Extension(name="pyamdgpuinfo.pyamdgpuinfo", sources=["pyamdgpuinfo
 
 setuptools.setup(
     name="pyamdgpuinfo",
-    version="1.0.1",
+    version="1.0.2",
     author="mark9064",
     description="AMD GPU stats",
     long_description=LONG_DESCRIPTION,
