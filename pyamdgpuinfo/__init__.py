630f0dad291b03689e2499253ca1cc0f070007a5
mark9064 <30447455+mark9064@users.noreply.github.com>
2020-04-14 00:18:28 +0100

    Update __init__ for new interface

--- pyamdgpuinfo/__init__.py
+++ pyamdgpuinfo/__init__.py
@@ -1,3 +1,4 @@
-from .pyamdgpuinfo import (setup_gpus, start_utilisation_polling, stop_utilisation_polling, cleanup,
-                           query_max_clocks, query_vram_usage, query_gtt_usage, query_sclk,
-                           query_mclk, query_temp, query_load, query_power, query_utilisation)
+# from .pyamdgpuinfo import (setup_gpus, start_utilisation_polling, stop_utilisation_polling, cleanup,
+#                            query_max_clocks, query_vram_usage, query_gtt_usage, query_sclk,
+#                            query_mclk, query_temp, query_load, query_power, query_utilisation)
+from .pyamdgpuinfo import (GPUInfo, get_gpu, detect_gpus)
