f92b708ef944a812df58b4abb5a055ea781642d2
mark9064 <30447455+mark9064@users.noreply.github.com>
2023-10-11 23:50:43 +0100

    Migrate CI to cibuildwheel, bump minimum Python

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/mark9064/pyamdgpuinfo",
     packages=setuptools.find_packages(),
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     ext_modules=cythonize(EXTENSIONS, language_level="3str"),
     zip_safe=False,
     classifiers=[
